87132746f35a2610e4bdb75c93cb4e9ddfd11ed8
Gritzko Daniel Kleiner <gritzkoo@hotmail.com>
2018-10-06 02:00:25 -0300

    diminuindo ainda mais a qtd de linhas

--- app/Api/Trans.php
+++ app/Api/Trans.php
@@ -15,18 +15,17 @@ class Trans
     {
         foreach (trans($name) as $chave => $valor) {
             $_chave = $name.'.'.$chave;
-            is_array($valor) 
-                ? $this->getValue($valor, $_chave)
-                : $this->list[$_chave] = $valor;
+            $this->getValue($valor, $_chave);
         }
         return $this;
     }
     private function getValue($valor, $_chave)
     {
-        foreach ($valor as $key => $val) {
-            is_array($val)
-                ? $this->getValue($val, ($_chave . '.' . $key))
-                : $this->list[$_chave . '.' . $key] = $val;
+        if(!is_array($valor)) $this->list[$_chave] = $valor;
+        else {
+            foreach ($valor as $key => $val) {
+                $this->getValue($val, ($_chave . '.' . $key));
+            }
         }
     }
 }
